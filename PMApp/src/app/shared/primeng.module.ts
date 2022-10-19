8dc3ef91fff421715ec08cc4f19ebe0a6b6158d4
Francisco Javier González del Solar <frangdelsolar@gmail.com>
2022-10-19 15:54:52 -0300

    dialog added

--- PMApp/src/app/shared/primeng.module.ts
+++ PMApp/src/app/shared/primeng.module.ts
@@ -3,11 +3,14 @@ import {ButtonModule} from 'primeng/button';
 import {CalendarModule} from 'primeng/calendar';
 import {CardModule} from 'primeng/card';
 import {DialogModule} from 'primeng/dialog';
+import {DynamicDialogModule} from 'primeng/dynamicdialog';
 import {DividerModule} from 'primeng/divider';
 import {DropdownModule} from 'primeng/dropdown';
 import {InputNumberModule} from 'primeng/inputnumber';
 import {InputSwitchModule} from 'primeng/inputswitch';
 import {InputTextModule} from 'primeng/inputtext';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
 import {SelectButtonModule} from 'primeng/selectbutton';
 import {TooltipModule} from 'primeng/tooltip';
 
@@ -19,9 +22,12 @@ const myModules = [
     DialogModule,
     DividerModule,
     DropdownModule,
+    DynamicDialogModule,
     InputNumberModule,
     InputSwitchModule,
     InputTextModule,
+    MessagesModule,
+    MessageModule,
     SelectButtonModule,
     TooltipModule
 ];
